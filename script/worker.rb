9ddd12a48a9146ba55fb5b169a723df792caf5b5
Julio Capote <jcapote@gmail.com>
2012-01-08 17:42:42 -0800

    working worker

--- script/worker.rb
+++ script/worker.rb
@@ -1,3 +1,4 @@
+require 'post'
 require "amqp"
 
 EventMachine.run do
@@ -7,8 +8,27 @@ EventMachine.run do
     channel  = AMQP::Channel.new(connection)
 
     channel.queue("posts.sanitize", :auto_delete => true).subscribe do |payload|
-      puts "Received a message: #{payload}. Disconnecting..."
+      p "[sanitize] finding post #{payload} "
+      po = Post.find(payload)
+      po.sanitize!
+      channel.direct("").publish(payload, :routing_key => "posts.process") 
     end
+    
+    channel.queue("posts.process", :auto_delete => true).subscribe do |payload|
+      p "[process] finding post #{payload}"
+      po = Post.find(payload)
+      po.process!
+      channel.direct("").publish(payload, :routing_key => "posts.autopost") 
+    end
+   
+    channel.queue("posts.autopost", :auto_delete => true).subscribe do |payload|
+      p "[autopost] finding post #{payload}"
+      po = Post.find(payload)
+      po.autopost!
+      p "finished post #{payload}"
+    end
+
+
 
   end
 end
