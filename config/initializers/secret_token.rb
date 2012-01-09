0b66dbf306f50c96d92b898ce065dc854c4cdbbc
twoism <signalstatic@gmail.com>
2012-01-08 16:35:10 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pipelined::Application.config.secret_token = '2d64f86c1c9372e390fb1b71be3dae94f4e97a11671bca79ef9ae3a721e6826b958d6d8dedaf2a7a1c03c2bfee68d3d6d10bd992e8f4846b6b82bf3126d2404c'
