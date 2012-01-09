1ca49679994025a0110fba6723292af9c8bb9bca
twoism <signalstatic@gmail.com>
2012-01-08 17:15:48 -0800

    bang methods for states

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+
+  def sanitize!
+    sanitized_at = DateTime.now
+    save!
+  end
+
+  def process!
+    processed_at = DateTime.now
+    save!
+  end
+
+  def autopost!
+    autoposted_at = DateTime.now
+    save!
+  end
 end
